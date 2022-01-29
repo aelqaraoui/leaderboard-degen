cc7182fae1eb29daa19ba43aa57d856c2707d2ce
Amine <aelqaraoui101@gmail.com>
2022-01-29 18:24:40 +0100

    A

---
+++ api/indexer.js
@@ -0,0 +1,56 @@
+const { Pool } = require('pg')
+const express = require('express')
+const router = express.Router()
+
+query = `
+        select 
+            date_trunc('minute', to_timestamp(block_timestamp/1000/1000/1000)) as time,
+            signer_account_id as signer,
+            receiver_account_id as receiver
+        from 
+            transactions t
+        where
+            receiver_account_id = 'coin-flip.woothugg.near'
+        `
+
+router.get('/', (req, result) => {
+
+    const pool = new Pool({
+        user: 'public_readonly',
+        host: 'mainnet.db.explorer.indexer.near.dev',
+        database: 'mainnet_explorer',
+        password: 'nearprotocol',
+    })
+
+    pool.query(query, (err, res) => {
+        if(!res) {
+            console.log(err)
+            res.end();
+            result.end();
+        }
+
+        console.log(!res);
+        let arr = res.rows.filter((val)=>val.receiver === 'coin-flip.woothugg.near').map((val)=>val.signer).filter((val)=>val !== 'coin-flip.woothugg.near')
+        const counts = {};
+    
+        for (const num of arr) {
+            counts[num] = counts[num] ? counts[num] + 1 : 1;
+        }
+        console.log(counts)
+    
+        let items = Object.keys(counts).map(function(key) {
+            return [key, counts[key]];
+        });
+        
+        items.sort(function(first, second) {
+            return second[1] - first[1];
+        });
+    
+        console.log(items);
+        
+        pool.end() 
+        result.json(items);
+    })
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { Pool } = require('pg')
 const express = require('express')
+const indexer = require('./api/indexer');
 const app = express()
 const port = 3000
 
@@ -8,55 +8,7 @@ app.use(cors({
     origin: '*'
 }));
 
-query = `
-        select 
-            date_trunc('minute', to_timestamp(block_timestamp/1000/1000/1000)) as time,
-            signer_account_id as signer,
-            receiver_account_id as receiver
-        from 
-            transactions t
-        where
-            receiver_account_id = 'coin-flip.woothugg.near'
-        `
-
-app.get('/', (req, result) => {
-
-    const pool = new Pool({
-        user: 'public_readonly',
-        host: 'mainnet.db.explorer.indexer.near.dev',
-        database: 'mainnet_explorer',
-        password: 'nearprotocol',
-    })
-
-    pool.query(query, (err, res) => {
-        if(res === undefined) {
-        
-            result.end();
-        }
-
-        console.log(res);
-        let arr = res.rows.filter((val)=>val.receiver === 'coin-flip.woothugg.near').map((val)=>val.signer).filter((val)=>val !== 'coin-flip.woothugg.near')
-        const counts = {};
-    
-        for (const num of arr) {
-            counts[num] = counts[num] ? counts[num] + 1 : 1;
-        }
-        console.log(counts)
-    
-        let items = Object.keys(counts).map(function(key) {
-            return [key, counts[key]];
-        });
-        
-        items.sort(function(first, second) {
-            return second[1] - first[1];
-        });
-    
-        console.log(items);
-        
-        pool.end() 
-        result.json(items);
-    })
-})
+app.use('/', indexer);
 
 app.listen(process.env.PORT || port, () => {
   console.log(`${port}`)
